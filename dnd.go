f326d0bf410a7a24a8ffa831d94f87438d4b5f40
Paul Ladyman <ladyman.paul@gmail.com>
2018-10-25 22:02:05 +0100

    move interactive ability check questions into AbilityCheckCli

--- dnd.go
+++ dnd.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
-	"os"
-	"strings"
 
 	"github.com/Paul-Ladyman/dndcli/dnd"
 	"github.com/Paul-Ladyman/dndcli/view"
@@ -15,11 +11,6 @@ func main() {
 	var ability = flag.String("abcheck", "", "Explain how to perform a check on the specified ability")
 	flag.Parse()
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Does the player have Advantage (adv), Disadvantage (dis) or neither (press enter)?")
-	circumstance, _ := reader.ReadString('\n')
-
-	abilityCheckSummary, _ := dnd.AbilityCheck(*ability, strings.TrimSpace(circumstance))
-
+	abilityCheckSummary, _ := dnd.AbilityCheckCli(*ability)
 	view.ExplainAbilityCheck(abilityCheckSummary)
 }
--- dnd/abilitycheck.go
+++ dnd/abilitycheck.go
@@ -1,6 +1,13 @@
 package dnd
 
-import "github.com/Paul-Ladyman/dndcli/domain"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Paul-Ladyman/dndcli/domain"
+)
 
 // AbilityCheck returns a summary of an ability check given an ability and an advantage circumstance
 func AbilityCheck(abilityString string, circumstanceString string) (domain.AbilityCheckSummary, error) {
@@ -14,4 +21,13 @@ func AbilityCheck(abilityString string, circumstanceString string) (domain.Abili
 		Throw:        domain.ThrowFactory(circumstance),
 		Target:       domain.DC,
 	}, nil
+}
+
+// AbilityCheckCli interactively retrieves information from the user relevant to ability checks
+func AbilityCheckCli(ability string) (domain.AbilityCheckSummary, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Does the player have Advantage (adv), Disadvantage (dis) or neither (press enter)?")
+	circumstance, _ := reader.ReadString('\n')
+
+	return AbilityCheck(ability, strings.TrimSpace(circumstance))
 }
